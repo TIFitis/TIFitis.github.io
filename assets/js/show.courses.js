bc91b283a40afb84676b09e99c1ad7e049ad0ddb
TIFitis <cs18mtech11023@iith.ac.in>
2020-10-06 00:34:30 +0530

    Added Skills

---
+++ assets/js/show.courses.js
@@ -0,0 +1,8 @@
+function show_courses() {
+    $("#my-courses").fadeIn("slow");
+    $("#my-bg").fadeIn("slow");
+}
+function hide_courses() {
+    $("#my-courses").fadeOut();
+    $("#my-bg").fadeOut();
+}
