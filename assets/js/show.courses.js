07cc301067331a5107d0fdf574a583505a3ef57c
TIFitis <cs18mtech11023@iith.ac.in>
2021-07-27 22:34:42 +0100

    Updated stuff

--- assets/js/show.courses.js
+++ assets/js/show.courses.js
@@ -6,8 +6,12 @@ function show_projects() {
     $("#my-projects").fadeIn();
     $("#my-bg").fadeIn();
 }
+function show_simba() {
+    $("#simba-pic").fadeIn();
+}
 function hide_all() {
     $("#my-courses").fadeOut();
     $("#my-projects").fadeOut();
+    $("#simba-pic").fadeOut();
     $("#my-bg").fadeOut();
 }
